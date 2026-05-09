66952d1073fb0796ce5b3a6ba3bf8d9afec3d4a9
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2026-05-09 01:30:56 -0300

    creando instancias de repository y service

--- com/clase8/Main.java
+++ com/clase8/Main.java
@@ -1,19 +1,24 @@
 package com.clase8;
 
-import com.clase8.exceptions.InvalidOptionException;
+import com.clase8.repository.ProductoRepository;
+import com.clase8.services.ProductoService;
 import com.clase8.ui.ConsoleUI;
-
-import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
         ConsoleUI console = new ConsoleUI();
+        ProductoRepository productoRepository = new ProductoRepository();
+        ProductoService productoService = new ProductoService(productoRepository);
 
         console.showMenu();
         int opcion;
         do {
             opcion = console.getOption();
-            System.out.println("La opción seleccionada es: " + opcion);
+
+            switch (opcion) {
+                case 1: productoService.addProducto();
+                case 2: console.showProducts(productoService.getProductos());
+            }
         } while (opcion != 5);
     }
 }
