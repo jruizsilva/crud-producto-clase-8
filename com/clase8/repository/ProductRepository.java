98e37c239ca4f3ec36356e52af24e5fad442611d
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2026-05-09 11:27:39 -0300

    rename file

---
+++ com/clase8/repository/ProductRepository.java
@@ -0,0 +1,55 @@
+package com.clase8.repository;
+
+import com.clase8.exceptions.NotFoundException;
+import com.clase8.models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepository {
+    List<Product> products;
+
+    public ProductRepository() {
+        this.products = new ArrayList<>();
+    }
+
+    public ProductRepository(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Product> getProductos() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public void addProduct(Product product) {
+        this.products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        this.products.remove(product);
+    }
+
+    public Product getProductById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        throw new NotFoundException("No se encontró el producto con el id: " + id);
+    }
+
+    public void updateProduct(int productId,
+                               Product product) {
+        for (Product p : products) {
+            if (p.getId() == productId) {
+                p.setName(product.getName());
+                p.setPrice(product.getPrice());
+                p.setStock(product.getStock());
+            }
+        }
+    }
+}
